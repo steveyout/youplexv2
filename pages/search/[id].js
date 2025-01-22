24c60a77678e142b750402000710ecc743ec908e
steveyout <youtsteve1@gmail.com>
2025-01-22 14:18:07 +0300

    added search functionality

--- pages/search/[id].js
+++ pages/search/[id].js
@@ -26,8 +26,10 @@ import { VideoPostCard, VideoPostsSort, VideoPostsSearch } from '@/sections/movi
 import EmptyContent from '@/components/EmptyContent';
 import InfiniteScroll from 'react-infinite-scroller';
 import { varFade } from '@/components/animate';
-import { MOVIES } from '@consumet/extensions';
 import { SearchForm } from "@/sections/forms";
+
+//movies
+import { MovieDb } from 'moviedb-promise';
 // ----------------------------------------------------------------------
 
 const SORT_OPTIONS = [
@@ -48,7 +50,7 @@ const RootStyle = styled('div')(({ theme }) => ({
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 
-Videos.getLayout = function getLayout(page) {
+movies.getLayout = function getLayout(page) {
   return <Layout variant="main">{page}</Layout>;
 };
 
@@ -66,7 +68,7 @@ const applySort = (posts, sortBy) => {
   }
   return posts;
 };
-export default function Videos({ data }) {
+export default function movies({ search,pages }) {
   const { themeStretch } = useSettings();
 
   const isMountedRef = useIsMountedRef();
@@ -74,24 +76,22 @@ export default function Videos({ data }) {
 
   const { id } = query;
 
-  const [videos, setVideos] = useState(data.results);
-  const [loading, setLoading] = useState(videos.length === 0);
-  const [url, setUrl] = useState(data.hasNextPage);
+  const [movies, setMovies] = useState(search);
+  const [loading, setLoading] = useState(movies.length === 0);
   let [page, setPage] = useState(1);
 
 
-  const getAllPosts = useCallback(async () => {
+  const getAllMovies = useCallback(async () => {
     try {
       setLoading(true)
-      if (videos && !videos.length) {
+      if (movies && !movies.length) {
         const response = await axios.get(`/api/search/`, {
           params: {query: id},
         });
 
         if (isMountedRef.current) {
-          setVideos(response.data.results);
+          setMovies(response.movies);
           setLoading(false);
-          setUrl(response.data.hasNextPage)
           setPage(page++);
         }
       } else {
@@ -103,14 +103,14 @@ export default function Videos({ data }) {
   }, [id]);
 
   useEffect(() => {
-    getAllPosts();
-  }, [getAllPosts]);
+    getAllMovies();
+  }, [getAllMovies]);
 
 
   ///more
   const handleLoadMore = useCallback(async () => {
     try {
-      if (!url || url === '') return;
+      if (page>=pages) return;
       if (page===1){
         setPage(page++);
       }
@@ -119,27 +119,26 @@ export default function Videos({ data }) {
       });
 
       if (isMountedRef.current) {
-        setVideos((prev) => [...prev, ...response.data.results]);
+        setMovies((prev) => [...prev, ...response.movies]);
         setPage(page++);
-        setUrl(response.data.hasNextPage);
       }
     } catch (error) {
       console.error(error);
     }
-  }, [url]);
+  }, [page, id, isMountedRef.current]);
 
   ////////////structured data
   const list=[]
-  videos&&videos.map((movie,index)=>{
+  movies&&movies.map((movie,index)=>{
     list.push({
       "@type": "ListItem",
       "position": index,
       "item": {
         "@type": "Movie",
-        "url": movie.id.includes('movie')?PATH_PAGE.movie(movie.id.split('movie/')[1]):PATH_PAGE.tv(movie.id.split('tv/')[1]),
+        "url": PATH_PAGE.movie(movie.title),
         "name": movie.title,
-        "image": movie.image,
-        "dateCreated": movie.releaseDate,
+        "image": process.env.TMDB_URL+movie.poster_path,
+        "dateCreated": movie.release_date,
       }
     })
   })
@@ -184,14 +183,14 @@ export default function Videos({ data }) {
           <InfiniteScroll
             pageStart={0}
             loadMore={handleLoadMore}
-            hasMore={url===true}
+            hasMore={page>=pages}
             threshold={2500}
             loader={
               <Grid container spacing={3} mt={1}>
                 {[...Array(8)].map((post, index) =>
                   post ? (
                     <Grid key={post.id} item xs={12} sm={6} md={3}>
-                      <VideoPostCard video={post} index={index} />
+                      <VideoPostCard movie={post} index={index} />
                     </Grid>
                   ) : (
                     <SkeletonPostItem key={index} />
@@ -205,13 +204,13 @@ export default function Videos({ data }) {
                 [...Array(12)].map((post, index) =>
                   post ? (
                     <Grid key={post.id} item xs={12} sm={6} md={3}>
-                      <VideoPostCard video={post} index={index} />
+                      <VideoPostCard movie={post} index={index} />
                     </Grid>
                   ) : (
                     <SkeletonPostItem key={index} />
                   )
                 )
-              ) : !loading && videos.length === 0 ? (
+              ) : !loading && movies.length === 0 ? (
                 <Grid item xs={12} sm={12} md={12}>
                 <Box sx={{ width:'100%', margin: 'auto', textAlign: 'center' }}>
                   <EmptyContent
@@ -225,9 +224,9 @@ export default function Videos({ data }) {
                 </Box>
                 </Grid>
               ) : (
-                videos.map((post, index) => (
+                movies.map((post, index) => (
                   <Grid key={post.id} item xs={12} sm={6} md={3}>
-                    <VideoPostCard video={post} index={index} />
+                    <VideoPostCard movie={post} index={index} />
                   </Grid>
                 ))
               )}
@@ -242,18 +241,21 @@ export default function Videos({ data }) {
 export async function getServerSideProps(context) {
   try {
     const id = context.params.id;
-    const flixhq = new MOVIES.FlixHQ();
-    const movies = await flixhq.search(id);
+    const moviedb = new MovieDb(process.env.TMDB_API_KEY);
+    const search=await moviedb.searchMulti({query:id});
+    const pages=search.total_pages;
+    const movies=search.results.filter( (item) => item.media_type !== "person");
     return {
       props: {
-        data: JSON.parse(JSON.stringify(movies)),
+        search: JSON.parse(JSON.stringify(movies)),
+        pages:pages
       }, // will be passed to the page component as props
     };
   } catch (error) {
     console.error('error loading data');
     return {
       props: {
-        data: [],
+        movies: [],
       }, // will be passed to the page component as props
     };
   }
